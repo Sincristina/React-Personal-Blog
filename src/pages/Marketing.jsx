702ef11954f61f52fb5d990c9c5fc4ce32011896
Sincristina <cristinasin06@yahoo.com>
2024-08-25 23:35:00 +0300

    first commit

---
+++ src/pages/Marketing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import MarketingComponent from '../components/MarketingComponent'
+
+
+const Marketing = () => {
+  return (
+    <div>
+      <Layout>
+        <MarketingComponent />
+      </Layout>
+    </div>
+  )
+}
+
+export default Marketing
